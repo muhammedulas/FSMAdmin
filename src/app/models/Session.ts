ba9fb99f6a56402b715e5d4ef7104bfc6fa356e2
muhammedulas <muhammedulas3333@gmail.com>
2022-05-11 12:44:09 +0300

    Session ViewModel class created

---
+++ src/app/models/Session.ts
@@ -0,0 +1,18 @@
+export class Session { 
+    id: number = 0;
+    startedAt: string = "";
+    userId: number = 0;
+    userNr: number = 0;
+    userName: string = "";
+    userIsAdmin: number = 0;
+    userERPToken: string = "";
+    userLogoId: number = 0;
+    worksapceId: number = 0;
+    workspaceTitle: string = "";
+    workspaceFirmNr: number = 0;
+    workspacePerNr: number = 0;
+    deviceId: number = 0;
+    deviceNr: number = 0;
+    deviceIdentifier: string = "";
+    deviceDefinition: string = "";
+}
