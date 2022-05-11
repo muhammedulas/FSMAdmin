dcfd9053b103f9b2687a0edb32a1304b36996f86
muhammedulas <muhammedulas3333@gmail.com>
2022-05-11 12:53:08 +0300

    Service methods created

--- src/app/services/component-services/sessions.service.ts
+++ src/app/services/component-services/sessions.service.ts
@@ -7,4 +7,19 @@ export class SessionsService {
 
     constructor(private http: HttpClient, private auth: AuthService) { }
 
+    getActiveSessions() {
+        var url = `${this.auth.getBaseUrl()}/api/admin/sessions`;
+        return this.http.get(url, { headers: this.auth.getGlobalHeader() });
+    }
+
+    getActiveSession(id: number) {
+        var url = `${this.auth.getBaseUrl()}/api/admin/sessions/${id}`;
+        return this.http.get(url, { headers: this.auth.getGlobalHeader() });
+    }
+
+    terminateSession(id: number) {
+        var url = `${this.auth.getBaseUrl()}/api/admin/sessions/${id}/terminate`;
+        return this.http.get(url, { headers: this.auth.getGlobalHeader() });
+    }
+
 }
