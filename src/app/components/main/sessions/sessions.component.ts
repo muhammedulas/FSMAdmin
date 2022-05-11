584d86fc5f74f316e81cea8d6936f10446dd404c
muhammedulas <muhammedulas3333@gmail.com>
2022-05-11 13:00:48 +0300

    Sessions component set-up

--- src/app/components/main/sessions/sessions.component.ts
+++ src/app/components/main/sessions/sessions.component.ts
@@ -1,4 +1,14 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { Session } from 'inspector';
+import { SessionsService } from 'src/app/services/component-services/sessions.service';
+import { NotifierService } from 'src/app/services/notifier.service';
+
+interface ISession {
+  dataSource: Session[];
+  displayedColumns: string[];
+  selection: SelectionModel<Session>;
+}
 
 @Component({
   selector: 'app-sessions',
@@ -6,8 +16,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sessions.component.scss']
 })
 export class SessionsComponent implements OnInit {
-
-  constructor() { }
+  public sessions: ISession = {
+    dataSource: [],
+    displayedColumns: [],
+    selection: new SelectionModel<Session>()
+  }
+  constructor(private service: SessionsService, private notifier: NotifierService) { }
 
   ngOnInit() {
   }
