0bcf3d7306e8674dcf51614488d0cc9cc72fc7ed
muhammedulas <muhammedulas3333@gmail.com>
2022-05-14 18:06:32 +0300

    search pipe improved

--- src/app/pipes/searchUser.ts
+++ src/app/pipes/searchUser.ts
@@ -6,14 +6,22 @@ import { User } from '../models/User';
 })
 export class SearchUserPipe implements PipeTransform {
 
-    transform(array: User[], searchString: string): User[] {
+    transform(array: User[], searchString: string, admin: number = 3): User[] {
         if (!array || !searchString) return array;
         else {
-            return array.filter(usr => {
-                usr.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()) ||
-                usr.surName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()) ||
-                usr.userName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
-            })
+            let temp: User[] = [];
+            temp = array.filter(user =>
+                user.userName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()) ||
+                user.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()) ||
+                user.surName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
+            )
+            if (admin == 0) {
+                temp = temp.filter(user => user.isAdmin == 0)
+            }
+            else if (admin == 1) {
+                temp = temp.filter(user => user.isAdmin == 1)
+            }
+            return temp
         }
     }
 
