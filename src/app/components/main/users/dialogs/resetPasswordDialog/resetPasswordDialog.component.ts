41cebc71e1e71aecd011f0e9a1c46ec4cf9fe185
muhammedulas <muhammedulas3333@gmail.com>
2022-07-13 13:18:20 +0300

    Password Reset Form Created

--- src/app/components/main/users/dialogs/resetPasswordDialog/resetPasswordDialog.component.ts
+++ src/app/components/main/users/dialogs/resetPasswordDialog/resetPasswordDialog.component.ts
@@ -1,7 +1,14 @@
 import { Component, Inject, Injectable, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UsersService } from 'src/app/services/component-services/users.service';
+import { NotifierService } from 'src/app/services/notifier.service';
 import { ConfirmPasswordValidator } from './confirmPasswordValidator';
+
+interface IResetPasswordForm {
+  NewPassword: string;
+  ConfirmNewPassword: string;
+}
 
 
 @Injectable()
@@ -14,10 +21,11 @@ export class ResetPasswordDialogComponent implements OnInit {
   public resetForm: FormGroup = new FormGroup({});
 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public id: number, private fb: FormBuilder) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public id: number, private fb: FormBuilder, private service: UsersService, private notifier: NotifierService, private dialogRef: MatDialogRef<ResetPasswordDialogComponent>) { }
 
   ngOnInit() {
     this.initializeForm();
+    console.log(this.id)
   }
   initializeForm() {
     this.resetForm = this.fb.group({
@@ -29,8 +37,24 @@ export class ResetPasswordDialogComponent implements OnInit {
   }
 
 
-  submit(form: FormGroup) {
-    console.log("test");
+  submit() {
+    let formValue: IResetPasswordForm = {
+      NewPassword: this.resetForm.controls["password"].value,
+      ConfirmNewPassword: this.resetForm.controls["confirmPassword"].value
+    };
+    let req = this.service.resetPassword(2, formValue).subscribe({
+      next: (res) => {
+        if (res == null) {
+          this.notifier.success_top_center("Kullanıcının parolası güncellendi");
+          this.dialogRef.close();
+        }
+        else {
+          this.notifier.warning_top_center(JSON.stringify(res));
+        }
+      },
+      error: (err) => { this.notifier.error_top_center(JSON.stringify(err.description)); },
+      complete: () => { req.unsubscribe() }
+    });
   }
 
   matchError() {
