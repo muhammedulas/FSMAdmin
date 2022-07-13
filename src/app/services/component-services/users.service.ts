645c5b1cb3c73a93e8a7d1604f1b8bd3ff138f40
muhammedulas <muhammedulas3333@gmail.com>
2022-07-13 13:19:37 +0300

    Reset method implemented

--- src/app/services/component-services/users.service.ts
+++ src/app/services/component-services/users.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { AuthService } from '../auth.service';
 
+interface IResetPasswordForm {
+  NewPassword: string;
+  ConfirmNewPassword: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,5 +23,11 @@ export class UsersService {
   getUser(id: number) {
     let url = `${this.auth.getBaseUrl()}/api/admin/users/${id}`;
     return this.http.get<User>(url, { headers: this.auth.getGlobalHeader() });
+  }
+
+  resetPassword(id: number, formValue: IResetPasswordForm) {
+    let url = `${this.auth.getBaseUrl()}/api/admin/users/${id}/resetPassword`;
+    let body = JSON.stringify(formValue);
+    return this.http.post(url, body, { headers: this.auth.getGlobalHeader().set("Content-Type", "application/json") });
   }
 }
